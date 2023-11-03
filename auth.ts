431337cb4bebe0175f11b2f985364cbccf096f8a
Andrei Assa <andrei.assa@gmail.com>
2023-11-03 10:23:14 -0400

    Added Google Auth

--- auth.ts
+++ auth.ts
@@ -1,34 +1,41 @@
-import NextAuth, { type DefaultSession } from 'next-auth'
+import NextAuth, {type DefaultSession} from 'next-auth'
+import GoogleProvider from 'next-auth/providers/google'
 import GitHub from 'next-auth/providers/github'
 
 declare module 'next-auth' {
-  interface Session {
-    user: {
-      /** The user's id. */
-      id: string
-    } & DefaultSession['user']
-  }
+    interface Session {
+        user: {
+            /** The user's id. */
+            id: string
+        } & DefaultSession['user']
+    }
 }
 
 export const {
-  handlers: { GET, POST },
-  auth,
-  CSRF_experimental // will be removed in future
+    handlers: {GET, POST},
+    auth,
+    CSRF_experimental // will be removed in future
 } = NextAuth({
-  providers: [GitHub],
-  callbacks: {
-    jwt({ token, profile }) {
-      if (profile) {
-        token.id = profile.id
-        token.image = profile.avatar_url || profile.picture
-      }
-      return token
+    providers: [
+        GitHub,
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET
+        })
+    ],
+    callbacks: {
+        jwt({token, profile}) {
+            if (profile) {
+                token.id = profile.id
+                token.image = profile.avatar_url || profile.picture
+            }
+            return token
+        },
+        authorized({auth}) {
+            return !!auth?.user // this ensures there is a logged in user for -every- request
+        }
     },
-    authorized({ auth }) {
-      return !!auth?.user // this ensures there is a logged in user for -every- request
+    pages: {
+        signIn: '/sign-in' // overrides the next-auth default signin page https://authjs.dev/guides/basics/pages
     }
-  },
-  pages: {
-    signIn: '/sign-in' // overrides the next-auth default signin page https://authjs.dev/guides/basics/pages
-  }
 })
